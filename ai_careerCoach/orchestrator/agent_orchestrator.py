72b7997ec9b0a8f80ee0fadeddbfd012adb210c5
Mokshik Saluja <mokshiksaluja@gmail.com>
2026-07-21 11:28:06 +0530

    adding the changes

--- ai_careerCoach/orchestrator/agent_orchestrator.py
+++ ai_careerCoach/orchestrator/agent_orchestrator.py
@@ -10,6 +10,7 @@ from model.agent_response import AgentResponse
 
 
 class AgentOrchestrator:
+    MAX_RETRIES=3
     def __init__(self,memory:SharedMemory,conversation_memory):
         self.memory=memory
         self.agents:List[BaseAgent]=[]
@@ -55,12 +56,38 @@ class AgentOrchestrator:
                 raise ValueError (f"Agent {agent_name} is not register")
             print(f"\n Step:{step}:Executing{agent.get_agent_name()} Agent....")
 
-            final_response=agent.execute()
+            # final_response=agent.execute()
+            final_response=self._execute_with_retry(agent)
+
 
             print(f"{agent.get_agent_name} completed successfully")
 
         print("Workflow Completed")
         return final_response
+    
+
+    def _execute_with_retry(self,agent:BaseAgent)->AgentResponse:
+        """
+        execute an ai agent with retry mechanisnmn"""
+
+        for attempt in range(1,self.MAX_RETRIES+1):
+
+            try:
+                print(f"Attempt {attempt}")
+                response=agent.execute()
+                print("Success")
+                return response
+            except Exception as ex:
+                print(f"Attempt {attempt} failed...")
+                print(ex)
+
+                if attempt ==self.MAX_RETRIES:
+                    raise RuntimeError(
+                        f"{agent.get_agent_name()} failed after"
+                        f"{self.MAX_RETRIES} retries..."
+                    )
+                print("Retring")
+
 
     
     
